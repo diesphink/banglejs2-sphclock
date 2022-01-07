7a3cd96ab0b3747496bfc669068cd9eaffe789f9
Diego Pereyra <diesphink@gmail.com>
2022-01-07 00:19:44 -0300

    Updates condicionais

--- sphclock.app.js
+++ sphclock.app.js
@@ -4,6 +4,12 @@ var drawTimeout;
 const CLOCK = 1;
 const CALENDAR = 2;
 
+const INIT = 0;
+const TIMER = 1;
+const CHARGE_CHANGE = 2;
+const LOCK_CHANGE = 3;
+const CALENDAR = 4;
+
 var state = {
   show: CLOCK,
   calendarDate: undefined,
@@ -16,24 +22,31 @@ let queueDraw = function () {
   if (drawTimeout) clearTimeout(drawTimeout);
   drawTimeout = setTimeout(function () {
     drawTimeout = undefined;
-    draw("timer");
+    draw(TIMER);
   }, 60000 - (Date.now() % 60000));
 };
 
-let draw = function (origin) {
+let draw = function (condition) {
   var date = new Date();
-  console.log(`Draw by ${origin}`);
+  console.log(`Draw with conditions: ${condition}`);
 
   g.reset();
 
   switch (state.show) {
     case CLOCK:
-      require("sphclock.background.js").drawBackground();
-      require("sphclock.clock.js").drawClock(date, 53);
-      require("sphclock.agenda.js").drawCalendar(date);
-      require("sphclock.lock.js").drawLocked();
-      require("sphclock.bateria.js").drawBattery(158, 7);
-      require("sphclock.weather.js").drawWeather();
+      if (condition == INIT) {
+        require("sphclock.background.js").drawBackground();
+        require("sphclock.agenda.js").drawCalendar(date);
+        require("sphclock.weather.js").drawWeather();
+      }
+
+      if (condition == INIT || condition == TIMER || condition == LOCK_CHANGE) {
+        require("sphclock.clock.js").drawClock(date, 53);
+        require("sphclock.lock.js").drawLocked();
+      }
+      if (condition == INIT || condition == TIMER || condition == CHARGE_CHANGE)
+        require("sphclock.bateria.js").drawBattery(158, 7);
+
       queueDraw();
       break;
     case CALENDAR:
@@ -48,36 +61,43 @@ let draw = function (origin) {
 g.clear();
 
 // draw immediately at first, queue update
-draw("initial");
+draw(INIT);
+
+function touched_date() {}
 
 Bangle.on("charging", function () {
-  draw("charging");
+  draw(CHARGE_CHANGE);
 });
 
 Bangle.on("lock", function () {
   if (state.show == CALENDAR) state.show = CLOCK;
-  draw("lock");
+  draw(LOCK_CHANGE);
 });
 
 Bangle.on("touch", function (button, xy) {
-  // console.log("touch", button, xy);
-  if (state.show == CLOCK && xy.x < 88 && xy.y < 53) {
-    Bangle.buzz(100, 0.1);
-    state.show = CALENDAR;
-    state.calendarDate = new Date();
-    draw("touch to calendar");
-    // E.showMessage("These are\nLots of\nLines", "My Title");
+  // On CLOCK
+  if (state.show == CLOCK) {
+    // Touch on date
+    if (xy.x < 88 && xy.y < 53) {
+      Bangle.buzz(100, 0.1);
+      state.show = CALENDAR;
+      state.calendarDate = new Date();
+      draw(CALENDAR);
+    }
   } else if (state.show == CALENDAR) {
     Bangle.buzz(100, 0.1);
     if (xy.y <= 40) {
+      // Prev month
       if (xy.x < 60)
         state.calendarDate.setMonth(state.calendarDate.getMonth() - 1);
+      // Next month
       if (xy.x > 116)
         state.calendarDate.setMonth(state.calendarDate.getMonth() + 1);
-      draw("change month on calendar");
+      draw(CALENDAR);
     } else {
+      // "Main" calendar, back to clock
       state.show = CLOCK;
-      draw("touch to clock");
+      draw(INIT);
     }
   }
 });
